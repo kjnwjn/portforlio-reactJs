accfca4f4861847dd8f8a1788ca9218bf893090e
TQ9Woaz9Lzp@HS <kjnwjnpham@gmail.com>
2022-12-17 18:41:35 +0700

    fix seo

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/global.scss';
-import App from './views/App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.scss";
+import App from "./views/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import { Helmet } from 'react-helmet'; 
+
 AOS.init();
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Helmet>
-      <meta property="og:title" content="Phạm Nguyễn Hoàng Quân" />
-      <meta property="og:description" content="My Portfolio" />
-      <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/vi/b/b0/Avatar-Teaser-Poster.jpg"  />
-      <meta property="og:image:type" content="image/jpg"></meta>
-    </Helmet>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/views/App.js
+++ src/views/App.js
@@ -4,13 +4,22 @@ import React from "react";
 import Portfolio from './portforlio/Portforlio';
 import About from './about/About';
 import Contact from './contact/Contact';
+import { Helmet } from "react-helmet";
 import {
   Routes,
   Route,
 } from "react-router-dom";
 import Static from './Static/Static';
 function App() {
+
   return (
+    <>
+    <Helmet>
+      <meta property="og:title" content="Phạm Nguyễn Hoàng Quân" />
+      <meta property="og:description" content="My Portfolio" />
+      <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/vi/b/b0/Avatar-Teaser-Poster.jpg" />
+      <meta property="og:image:type" content="image/jpg"></meta>
+  </Helmet>
     <Routes>
     <Route path="/" element={<Static />}>
     <Route index element={<Home />} />
@@ -19,6 +28,7 @@ function App() {
     <Route path="/contact" element={<Contact />} />
     </Route>
   </Routes>
+  </>
 
   );
 }
